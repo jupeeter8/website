6ed9f7896ff404eab25d645eaa2d2782891c3240
jupeeter8 <69347989+jupeeter8@users.noreply.github.com>
2025-11-30 18:02:01 +0530

    Adds Image caption, css fixes mdx support and spotify embed

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/blog" }),
     schema: z.object({
       title: z.string(),
       description: z.string(),
